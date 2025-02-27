b45d318579be3c25df21bce94536e8bb160e716b
2023dfff <1374490218@qq.com>
2025-02-27 21:43:46 +0800

    change logger

--- benchmarking/utils.py
+++ benchmarking/utils.py
@@ -103,8 +103,10 @@ class Logger(object):
             print(f'Run {run + 1:02d}:')
             print(f'Highest Train: {result[:, 0].max():.2f}')
             print(f'Highest Valid: {result[:, 1].max():.2f}')
-            print(f'  Final Train: {result[argmax, 0]:.2f}')
-            print(f'   Final Test: {result[argmax, 2]:.2f}')
+            # print(f'  Final Train: {result[argmax, 0]:.2f}')
+            print(f'  Final Train: {result[:, 0].mean():.2f}')
+            # print(f'   Final Test: {result[argmax, 2]:.2f}')
+            print(f'   Final Test: {result[:, 2].mean():.2f}')
         else:
             best_results = []
 
